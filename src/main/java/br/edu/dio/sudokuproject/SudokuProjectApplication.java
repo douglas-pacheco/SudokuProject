dbba697b4a7d0356c1de2f526ed07946f269ab96
Douglas Pacheco <douglas.pacheco@edu.pucrs.br>
2025-05-29 19:31:03 -0300

    first commit

---
+++ src/main/java/br/edu/dio/sudokuproject/SudokuProjectApplication.java
@@ -0,0 +1,13 @@
+package br.edu.dio.sudokuproject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SudokuProjectApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SudokuProjectApplication.class, args);
+    }
+
+}
---
+++ src/test/java/br/edu/dio/sudokuproject/SudokuProjectApplicationTests.java
@@ -0,0 +1,13 @@
+package br.edu.dio.sudokuproject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SudokuProjectApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
